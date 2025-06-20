958167566ea5db59eb1f0c24d244128d4f066a72
salesmiguelz <miguelsales02@gmail.com>
2025-06-20 12:20:53 -0300

    feat: summary and values formatting

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -4,6 +4,7 @@ import { Summary } from "./components/Summary";
 import { PriceHighLight, TransactionsContainer, TransactionsTable } from "./styles";
 import { TransactionsContext } from "../../contexts/TransactionsContext";
 import { useContext } from "react";
+import { dateFormatter, priceFormatter } from "../../utils/formatter";
 
 
 export function Transactions() {
@@ -23,11 +24,12 @@ export function Transactions() {
                                     <td width='50%'>{transaction.description}</td>
                                     <td>
                                         <PriceHighLight variant={transaction.type}>
-                                            $ {transaction.price}
+                                            {transaction.type === 'outcome' && '-'}
+                                            {priceFormatter.format(transaction.price)}
                                         </PriceHighLight>
                                     </td>
                                     <td>{transaction.category}</td>
-                                    <td>{transaction.createdAt}</td>
+                                    <td>{dateFormatter.format(new Date(transaction.createdAt))}</td>
                                 </tr>
                             )
                         })}
